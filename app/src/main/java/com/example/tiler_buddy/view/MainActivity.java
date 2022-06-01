6141c04702bce198987f737508d1c1dcf10d0e73
krivanpeter <krivan.peter@outlook.com>
2022-06-01 18:00:31 +0100

    MainActivity's variables' names changed for cenvention

--- app/src/main/java/com/example/tiler_buddy/view/MainActivity.java
+++ app/src/main/java/com/example/tiler_buddy/view/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.tiler_buddy.view;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewManager;
@@ -36,16 +35,16 @@ public class MainActivity extends AppCompatActivity {
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         LinearLayout container = MainActivity.this.findViewById(R.id.container);
 
-        TextView button_obs = findViewById(R.id.button_obs);
-        button_obs.setOnClickListener(new Button.OnClickListener() {
+        TextView buttonObs = findViewById(R.id.button_obs);
+        buttonObs.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
                 View obstacle = inflater.inflate(R.layout.obstacle_layout, container, false);
                 inflater.inflate(R.layout.obstacle_layout, container, false);
                 container.addView(obstacle);
 
-                ImageButton delete_obs = obstacle.findViewById(R.id.delete_obs);
-                delete_obs.setOnClickListener(new Button.OnClickListener() {
+                ImageButton deleteObs = obstacle.findViewById(R.id.delete_obs);
+                deleteObs.setOnClickListener(new Button.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         ((ViewManager) obstacle.getParent()).removeView(obstacle);
@@ -54,45 +53,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        final Button button_calc = findViewById(R.id.button_calc);
-        button_calc.setOnClickListener(new View.OnClickListener() {
+        final Button buttonCalculate = findViewById(R.id.button_calc);
+        buttonCalculate.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Wall Inputs
-                EditText wall_length_in = findViewById(R.id.wall_length_in);
-                EditText wall_height_in = findViewById(R.id.wall_height_in);
+                EditText wallLengthIn = findViewById(R.id.wall_length_in);
+                EditText wallHeightIn = findViewById(R.id.wall_height_in);
                 // Tile Inputs
-                EditText tile_length_in = findViewById(R.id.tile_length_in);
-                EditText tile_height_in = findViewById(R.id.tile_height_in);
+                EditText tileLengthIn = findViewById(R.id.tile_length_in);
+                EditText tileHeightIn = findViewById(R.id.tile_height_in);
                 // Spacer width
-                EditText spacer_width_in = findViewById(R.id.spacer_width_in);
+                EditText spacerWidthIn = findViewById(R.id.spacer_width_in);
                 // 10% wastage
-                SwitchMaterial ten_percent_in = findViewById(R.id.ten_percent);
+                SwitchMaterial tenPercentIn = findViewById(R.id.ten_percent);
 
                 try {
-                    List<Obstacle> obs = parseInputsAsObstacles();
+                    List<Obstacle> obstacles = parseInputsAsObstacles();
                     //Getting inputs as strings for checks
-                    String wall_length_str = wall_length_in.getText().toString();
-                    String wall_height_str = wall_height_in.getText().toString();
-                    String tile_length_str = tile_length_in.getText().toString();
-                    String tile_height_str = tile_height_in.getText().toString();
-                    String spacer_width_str = spacer_width_in.getText().toString();
+                    String wallLengthStr = wallLengthIn.getText().toString();
+                    String wallHeightStr = wallHeightIn.getText().toString();
+                    String tileLengthStr = tileLengthIn.getText().toString();
+                    String tileHeightStr = tileHeightIn.getText().toString();
+                    String spacerWidthStr = spacerWidthIn.getText().toString();
                     //Checking if inputs are empty
-                    if (wall_length_str.isEmpty() || wall_height_str.isEmpty() || tile_length_str.isEmpty() || tile_height_str.isEmpty() || spacer_width_str.isEmpty()) {
+                    if (wallLengthStr.isEmpty() || wallHeightStr.isEmpty() || tileLengthStr.isEmpty() || tileHeightStr.isEmpty() || spacerWidthStr.isEmpty()) {
                         Toast.makeText(getApplicationContext(), "You did not enter all numbers", Toast.LENGTH_SHORT).show();
                     } else {
                         // Getting All User Inputs
-                        boolean ten_percent = ten_percent_in.isChecked();
+                        boolean tenPercent = tenPercentIn.isChecked();
                         // Converting strings to integers
-                        WallDimensions wallDimensions = new WallDimensions(Integer.parseInt(wall_length_str), Integer.parseInt(wall_height_str));
-                        TileDimensions tileDimensions = new TileDimensions(Integer.parseInt(tile_length_str), Integer.parseInt(tile_height_str));
+                        WallDimensions wallDimensions = new WallDimensions(Integer.parseInt(wallLengthStr), Integer.parseInt(wallHeightStr));
+                        TileDimensions tileDimensions = new TileDimensions(Integer.parseInt(tileLengthStr), Integer.parseInt(tileHeightStr));
                         // int spacer_width_int = Integer.parseInt(spacer_width_str);
                         // Calculating values
-                        double toBeTiledArea = Calculator.calculateToBeTiledArea(wallDimensions, Calculator.calculateObstacleArea(obs));
-                        double numTiles = Calculator.calculateTiles(ten_percent, toBeTiledArea, tileDimensions);
+                        double toBeTiledArea = Calculator.calculateToBeTiledArea(wallDimensions, Calculator.calculateObstacleArea(obstacles));
+                        double numTiles = Calculator.calculateTiles(tenPercent, toBeTiledArea, tileDimensions);
                         double wallArea = Calculator.convertToMeter(toBeTiledArea);
-
-                        Log.d("done", String.valueOf(toBeTiledArea));
-                        Log.d("done", String.valueOf(numTiles));
                         // Start New Activity
                         Intent intent = new Intent(MainActivity.this, CalculatedActivity.class);
                         intent.putExtra("data", new CalculatedValuesWrapper(wallArea, numTiles));
@@ -107,43 +103,43 @@ public class MainActivity extends AppCompatActivity {
 
     // Setting up Obstacles
     public List<Obstacle> parseInputsAsObstacles() throws ObstacleInputException {
-        List<Obstacle> obs_ins = new ArrayList<>();
+        List<Obstacle> obstacleIns = new ArrayList<>();
         LinearLayout obstaclesContainer = findViewById(R.id.container);
 
         for (int i = 0; i < obstaclesContainer.getChildCount(); i++) {
             View child = obstaclesContainer.getChildAt(i);
 
-            EditText obs_length_in = child.findViewById(R.id.obs_length_in);
-            EditText obs_height_in = child.findViewById(R.id.obs_height_in);
-            EditText obs_from_left = child.findViewById(R.id.obs_from_left);
-            EditText obs_from_bot = child.findViewById(R.id.obs_from_bot);
+            EditText obsLengthIn = child.findViewById(R.id.obs_length_in);
+            EditText obsHeightIn = child.findViewById(R.id.obs_height_in);
+            EditText obsDisFromLeft = child.findViewById(R.id.obs_from_left);
+            EditText obsDisFromBottom = child.findViewById(R.id.obs_from_bot);
 
             if (isObstaclesInputValid(child)) {
                 Obstacle obstacle = new Obstacle();
-                obstacle.setLength(getEditTextNumbers(obs_length_in));
-                obstacle.setHeight(getEditTextNumbers(obs_height_in));
-                obstacle.setDisLeft(getEditTextNumbers(obs_from_left));
-                obstacle.setDisBot(getEditTextNumbers(obs_from_bot));
-                obs_ins.add(obstacle);
+                obstacle.setLength(getEditTextNumbers(obsLengthIn));
+                obstacle.setHeight(getEditTextNumbers(obsHeightIn));
+                obstacle.setDisLeft(getEditTextNumbers(obsDisFromLeft));
+                obstacle.setDisBot(getEditTextNumbers(obsDisFromBottom));
+                obstacleIns.add(obstacle);
             } else {
                 throw new ObstacleInputException();
             }
         }
-        return obs_ins;
+        return obstacleIns;
     }
 
     private boolean isObstaclesInputValid(View obstacleInput) {
-        EditText obs_length_in = obstacleInput.findViewById(R.id.obs_length_in);
-        EditText obs_height_in = obstacleInput.findViewById(R.id.obs_height_in);
-        EditText obs_from_left = obstacleInput.findViewById(R.id.obs_from_left);
-        EditText obs_from_bot = obstacleInput.findViewById(R.id.obs_from_bot);
+        EditText obsLengthIn = obstacleInput.findViewById(R.id.obs_length_in);
+        EditText obsHeightIn = obstacleInput.findViewById(R.id.obs_height_in);
+        EditText obsDisFromLeft = obstacleInput.findViewById(R.id.obs_from_left);
+        EditText obsDisFromBottom = obstacleInput.findViewById(R.id.obs_from_bot);
 
-        String obs_length_in_txt = obs_length_in.getText().toString();
-        String obs_height_in_txt = obs_height_in.getText().toString();
-        String obs_from_left_txt = obs_from_left.getText().toString();
-        String obs_from_bot_txt = obs_from_bot.getText().toString();
+        String obsLengthInTxt = obsLengthIn.getText().toString();
+        String obsHeightInTxt = obsHeightIn.getText().toString();
+        String obsDisFromLeftTxt = obsDisFromLeft.getText().toString();
+        String obsDisFromBottomTxt = obsDisFromBottom.getText().toString();
 
-        return !obs_length_in_txt.isEmpty() && !obs_height_in_txt.isEmpty() && !obs_from_left_txt.isEmpty() && !obs_from_bot_txt.isEmpty();
+        return !obsLengthInTxt.isEmpty() && !obsHeightInTxt.isEmpty() && !obsDisFromLeftTxt.isEmpty() && !obsDisFromBottomTxt.isEmpty();
     }
 
     private int getEditTextNumbers(EditText editText) {
