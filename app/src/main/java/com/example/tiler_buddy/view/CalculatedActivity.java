269f45de6eee6c6508d956804a34a42680c26761
krivanpeter <krivan.peter@outlook.com>
2022-06-23 18:03:01 +0100

    refactoring

--- app/src/main/java/com/example/tiler_buddy/view/CalculatedActivity.java
+++ app/src/main/java/com/example/tiler_buddy/view/CalculatedActivity.java
@@ -38,60 +38,64 @@ public class CalculatedActivity extends AppCompatActivity {
         WallDimensions wallDimensions = calculatedValuesWrapper.getWallDimensions();
 
         ImageView imgview = findViewById(R.id.imageView1);
-
         if (imgview != null) {
-            Paint paint = new Paint();
-            paint.setColor(Color.parseColor("#000000"));
-            paint.setStyle(Paint.Style.STROKE);
+            Bitmap bg = Bitmap.createBitmap(wallDimensions.getLength() + 10, wallDimensions.getHeight() - 10, Bitmap.Config.ARGB_8888);
+            Canvas canvas = createCanvas(bg);
+            Paint paint = setUpPaint();
+            drawTiles(canvas, paint, tiles);
+            imgview.setBackground(new BitmapDrawable(bg));
+        }
+    }
 
-            Display display = getWindowManager().getDefaultDisplay();
-            Point size = new Point();
-            display.getSize(size);
-            int displayWidth = size.x;
-            int displayHeight = size.y;
+    public void drawTiles(Canvas canvas, Paint paint, List<List<Tile>> tiles) {
+        for (int i = 0; i < tiles.size(); i++) {
+            for (int j = 0; j < tiles.get(i).size(); j++) {
+                Tile tile = tiles.get(i).get(j);
+                int posx1 = tile.getX1();
+                int posy1 = tile.getY1();
+                int posx2 = tile.getX2();
+                int posy2 = tile.getY2();
 
-
-            Bitmap bg = Bitmap.createBitmap(wallDimensions.getLength() + 10, wallDimensions.getHeight() + 10, Bitmap.Config.ARGB_8888);
-
-            Canvas canvas = new Canvas(bg);
-
-            for (int i = 0; i < tiles.size(); i++) {
-                for (int j = 0; j < tiles.get(i).size(); j++) {
-                    Tile tile = tiles.get(i).get(j);
-                    int posx1 = tile.getX1();
-                    int posy1 = tile.getY1();
-                    int posx2 = tile.getX2();
-                    int posy2 = tile.getY2();
-                    String length = String.valueOf(tile.getLength());
-                    String height = String.valueOf(tile.getHeight());
-
-                    if (tile.getSides().isEmpty()) {
-                        canvas.drawRect(posx1, posy1, posx2, posy2, paint);
-                    } else {
-                        for (int x = 0; x < tile.getSides().size(); x++) {
-                            String a1 = String.valueOf(tile.getSides().get(x).getX1());
-                            String a2 = String.valueOf(tile.getSides().get(x).getY1());
-                            String a3 = String.valueOf(tile.getSides().get(x).getX2());
-                            String a4 = String.valueOf(tile.getSides().get(x).getY2());
-                            canvas.drawLine(tile.getSides().get(x).getX1(), tile.getSides().get(x).getY1(), tile.getSides().get(x).getX2(), tile.getSides().get(x).getY2(), paint);
-                        }
+                if (tile.getSides().isEmpty()) {
+                    canvas.drawRect(posx1, posy1, posx2, posy2, paint);
+                } else {
+                    for (int x = 0; x < tile.getSides().size(); x++) {
+                        canvas.drawLine(tile.getSides().get(x).getX1(), tile.getSides().get(x).getY1(), tile.getSides().get(x).getX2(), tile.getSides().get(x).getY2(), paint);
                     }
                 }
             }
-            imgview.setBackground(new BitmapDrawable(bg));
         }
+    }
 
+    private Paint setUpPaint() {
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#000000"));
+        paint.setStyle(Paint.Style.STROKE);
+        return paint;
+    }
+
+    private Canvas createCanvas(Bitmap bg) {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
         /*
-        button = findViewById(R.id.button1);
-        //Request for camera runtime permission
-        if (ContextCompat.checkSelfPermission(CalculatedActivity.this, Manifest.permission.CAMERA)
-        != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(CalculatedActivity.this, new String[]{
-                    Manifest.permission.CAMERA
-            }, 100);
-        }
-        */
+        int displayWidth = size.x;
+        int displayHeight = size.y;
+         */
+
+        Canvas canvas = new Canvas(bg);
+        return canvas;
     }
 }
+/*
+    button = findViewById(R.id.button1);
+    //Request for camera runtime permission
+    if (ContextCompat.checkSelfPermission(CalculatedActivity.this, Manifest.permission.CAMERA)
+    != PackageManager.PERMISSION_GRANTED) {
+        ActivityCompat.requestPermissions(CalculatedActivity.this, new String[]{
+                Manifest.permission.CAMERA
+        }, 100);
+    }
+*/
 
 
