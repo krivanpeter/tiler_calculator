3193061edfd00f41ee6969b4de0541afd09657ed
krivanpeter <krivan.peter@outlook.com>
2022-06-26 22:30:04 +0100

    added setter for x1 position

--- app/src/main/java/com/pk/tiler_buddy/Rectangle.java
+++ app/src/main/java/com/pk/tiler_buddy/Rectangle.java
@@ -30,12 +30,20 @@ public class Rectangle implements Serializable {
         return x1;
     }
 
+    public void setX1(int x1) {
+        this.x1 = x1;
+    }
+
     public int getY1() {
         return y1;
     }
 
     public int getX2() {
         return x2;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
     }
 
     public int getY2() {
