ff8a13d3432fd4092c175d28eb865b9f2b978881
krivanpeter <krivan.peter@outlook.com>
2022-06-27 18:24:54 +0100

    new method to check shifted tile overlapping. Methods simplified

--- app/src/main/java/com/pk/tiler_buddy/Overlap.java
+++ app/src/main/java/com/pk/tiler_buddy/Overlap.java
@@ -35,102 +35,70 @@ public class Overlap {
         if (tile.getY1() >= obstacle.getY2() || tile.getY2() <= obstacle.getY1()) {
             return false;
         }
-        if (tile.getX1() >= obstacle.getX2() || tile.getX2() <= obstacle.getX1()) {
-            return false;
-        }
-        return true;
+        return tile.getX1() < obstacle.getX2() && tile.getX2() > obstacle.getX1();
     }
 
     public static boolean isFullyOverlapping(Tile tile, Obstacle obstacle) {
-        if (isOverlapping(tile, obstacle) &&
+        return isOverlapping(tile, obstacle) &&
                 tile.getX1() >= obstacle.getX1() && tile.getX2() <= obstacle.getX2() &&
-                tile.getY1() >= obstacle.getY1() && tile.getY2() <= obstacle.getY2()) {
-            return true;
+                tile.getY1() >= obstacle.getY1() && tile.getY2() <= obstacle.getY2();
+    }
+
+    static boolean isXOverlapping(Tile tile, Obstacle obstacle, int point) {
+        if (tile.getY1() >= obstacle.getY2() || tile.getY2() <= obstacle.getY1()) {
+            return false;
         }
-        return false;
+        if (point >= obstacle.getX2() || point <= obstacle.getX1()) {
+            return false;
+        }
+        return !isBottomLeftCornerOverlapping(tile, obstacle) && !isTopLeftCornerOverlapping(tile, obstacle);
     }
 
     private static boolean isLeftOverlapping(Tile tile, Obstacle obstacle) {
-        if (isOverlapping(tile, obstacle) && tile.getX1() < obstacle.getX2() && tile.getX2() > obstacle.getX2()) {
-            return true;
-        }
-        return false;
+        return isOverlapping(tile, obstacle) && tile.getX1() < obstacle.getX2() && tile.getX2() > obstacle.getX2();
     }
 
     private static boolean isOnlyLeftOverlapping(Tile tile, Obstacle obstacle) {
-        if (isLeftOverlapping(tile, obstacle) && !isBottomOverlapping(tile, obstacle) && !isTopOverlapping(tile, obstacle)) {
-            return true;
-        }
-        return false;
+        return isLeftOverlapping(tile, obstacle) && !isBottomOverlapping(tile, obstacle) && !isTopOverlapping(tile, obstacle);
     }
 
     private static boolean isRightOverlapping(Tile tile, Obstacle obstacle) {
-        if (isOverlapping(tile, obstacle) && tile.getX1() < obstacle.getX1() && tile.getX2() > obstacle.getX1()) {
-            return true;
-        }
-        return false;
+        return isOverlapping(tile, obstacle) && tile.getX1() < obstacle.getX1() && tile.getX2() > obstacle.getX1();
     }
 
     private static boolean isOnlyRightOverlapping(Tile tile, Obstacle obstacle) {
-        if (isRightOverlapping(tile, obstacle) && !isBottomOverlapping(tile, obstacle) && !isTopOverlapping(tile, obstacle)) {
-            return true;
-        }
-        return false;
+        return isRightOverlapping(tile, obstacle) && !isBottomOverlapping(tile, obstacle) && !isTopOverlapping(tile, obstacle);
     }
 
     private static boolean isBottomOverlapping(Tile tile, Obstacle obstacle) {
-        if (isOverlapping(tile, obstacle) && tile.getY1() < obstacle.getY2() && tile.getY2() > obstacle.getY2()) {
-            return true;
-        }
-        return false;
+        return isOverlapping(tile, obstacle) && tile.getY1() < obstacle.getY2() && tile.getY2() > obstacle.getY2();
     }
 
     private static boolean isOnlyBottomOverlapping(Tile tile, Obstacle obstacle) {
-        if (isBottomOverlapping(tile, obstacle) && !isLeftOverlapping(tile, obstacle) && !isRightOverlapping(tile, obstacle)) {
-            return true;
-        }
-        return false;
+        return isBottomOverlapping(tile, obstacle) && !isLeftOverlapping(tile, obstacle) && !isRightOverlapping(tile, obstacle);
     }
 
     private static boolean isTopOverlapping(Tile tile, Obstacle obstacle) {
-        if (isOverlapping(tile, obstacle) && tile.getY1() < obstacle.getY1() && tile.getY2() > obstacle.getY1()) {
-            return true;
-        }
-        return false;
+        return isOverlapping(tile, obstacle) && tile.getY1() < obstacle.getY1() && tile.getY2() > obstacle.getY1();
     }
 
     private static boolean isOnlyTopOverlapping(Tile tile, Obstacle obstacle) {
-        if (isTopOverlapping(tile, obstacle) && !isLeftOverlapping(tile, obstacle) && !isRightOverlapping(tile, obstacle)) {
-            return true;
-        }
-        return false;
+        return isTopOverlapping(tile, obstacle) && !isLeftOverlapping(tile, obstacle) && !isRightOverlapping(tile, obstacle);
     }
 
     private static boolean isBottomLeftCornerOverlapping(Tile tile, Obstacle obstacle) {
-        if (isBottomOverlapping(tile, obstacle) && isLeftOverlapping(tile, obstacle)) {
-            return true;
-        }
-        return false;
+        return isBottomOverlapping(tile, obstacle) && isLeftOverlapping(tile, obstacle);
     }
 
-    private static boolean isTopLeftCornerOverlapping(Tile tile, Obstacle obstacle) {
-        if (isTopOverlapping(tile, obstacle) && isLeftOverlapping(tile, obstacle)) {
-            return true;
-        }
-        return false;
+    static boolean isTopLeftCornerOverlapping(Tile tile, Obstacle obstacle) {
+        return isTopOverlapping(tile, obstacle) && isLeftOverlapping(tile, obstacle);
     }
 
     private static boolean isTopRightCornerOverlapping(Tile tile, Obstacle obstacle) {
-        if (isTopOverlapping(tile, obstacle) && isRightOverlapping(tile, obstacle)) {
-            return true;
-        }
-        return false;
+        return isTopOverlapping(tile, obstacle) && isRightOverlapping(tile, obstacle);
     }
 
     private static boolean isBottomRightCornerOverlapping(Tile tile, Obstacle obstacle) {
-        if (isBottomOverlapping(tile, obstacle) && isRightOverlapping(tile, obstacle)) {
-            return true;
-        }
-        return false;
+        return isBottomOverlapping(tile, obstacle) && isRightOverlapping(tile, obstacle);
     }
 }
